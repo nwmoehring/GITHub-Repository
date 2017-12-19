c7dfc299ed11cff0e57be0c8aa66d0f6517058a5
nwmoehring <34487878+nwmoehring@users.noreply.github.com>
2017-12-19 17:36:41 -0600

    Finished project 2
    Added array with guesses in it, displayed number of guess at the end. Removed unessisary end output after number of attempts and removed unessisary test of correctness of guess after the while loop.

--- P2_SECERET_NUMBER_GUESSING.py
+++ P2_SECERET_NUMBER_GUESSING.py
@@ -1,34 +1,39 @@
 '''
 Write a guessing game where the user has to guess a secret number
-After every guess the program tells the user if their number was too large or too smal
+After every guess the program tells the user if their number was too large or too small
 At the end the number of tries needed should be printed
-Guess of the same number count as only one guess
+Guesses of the same number should count as only one guess
 '''
 
-#
-#IMPORTING, GETTING A MYSTERY NUMBER, & INPUT
-
 import random
-#RAND NUMB GEN LIBRARY & THE random() FUNCT IS CALLED FROM LIBRARY random WITH A .
-mystery_number = int(1000 * random.random())
-#random PRODUCES FLOATING POINT, * BY UPPER BOUND & CONVERT TO INTEGER
+#RAND NUMBER GENERATOR LIBRARY & THE random() FUNCT IS CALLED FROM LIBRARY random WITH A .
+
+mystery_number = int(5 * random.random())
+#random PRODUCES FLOATING POINT, MULTIPLY BY UPPER BOUND & CONVERT TO INTEGER
+
 guess = int(input("Guess a number!"))
 #SPECIFY THE INPUT AS AN INTEGER OR IT WILL BE A STRING
 
-#
-#COMPAIRING MYSTERY NUMBER TO USER GUESSES
+guess_counter = [guess]
+#CREATING ARRAY FOR COUNTING NUMBER OF TRIES AND POPULATING WITH FIRST GUESS
 
 while mystery_number != guess:
-#IF THE GUESS IS NOT CORRECT IF STATEMENT BELOW WILL TEST IF NUMBER IS TOO SMALL
+#IF THE GUESS IS NOT CORRECT THE if STATEMENT BELOW WILL TEST SEE NUMBER IS TOO SMALL
 
     if mystery_number > guess:
         print("Guess was too small.")
         guess = int(input("Guess a another number!"))
-    elif mystery_number < guess:
-        #CODE WORKS WITH IF, ELIF USED TO SKIP CHEKING THE NUMBER AGAIN
+    else:
+        #CODE WORKS WITH IF AND ANOTHER TEST, ELSE USED TO SKIP CHEKING THE NUMBER AGAIN
         print("Guess was too big.")
         guess = int(input("Guess a another number!"))
 
-if mystery_number == guess:
-    print("You are correct!")
-#STATEMENT WONT PRINT UNLESS ITS RIGHT ON FIRST TRY IF THIS IS AT BEGINNING
+    if not guess in guess_counter:
+    #IF CURRENT VALUE OF GUESS IS NOT IN THE ARRAY IT WILL RUN THE CODE BELOW
+        guess_counter.append(guess)
+        #POPULATES ARRAY WITH CURRENT VALUE OF guess AFTER EVERY ATTEMPT
+
+print("Congratulations, it only took you "+str(len(guess_counter))+" attempts.")
+#STRING OUTPUT BROKEN UP BY THE LENGTH OF THE guess_counter ARRAY NEEDS CONCATONATORS +
+#DONT NEED TO TEST IF guess IS CORRECT B/C IT ALREADY BROKE OUT OF THE while LOOP
+    #ALSO CANT TEST IF guess IS CORRECT AT START B/C IT ONLY WORKS ON FIRST TRY
